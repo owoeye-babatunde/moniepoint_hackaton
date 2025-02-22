6509ccbb59bbd41b639f079ae67f2bd8696bffcf
owoeye-babatunde <owoeyebabatunde01@gmail.com>
2025-02-22 04:09:42 -0800

    Planned the entire project in detail
    Started the SalesAnalytics class implementation
    Planned all methods and function that need to be implemented;
    Methods of the class includes;
    parse_products
    process_line
    process_file
    calculate_hourly_average
    generate_report
    
    Then the function 'analyze_transactions' is to be implemented.
    It will use all available methods of the class to analyze transactions and answer all required questions.

--- solution/database_reader/run.py
+++ solution/database_reader/run.py
@@ -1,6 +1,47 @@
-def main():
-    print("Hello from database-reader!")
+import os
+from datetime import datetime
+from collections import defaultdict
 
+class SalesAnalytics:
+    def __init__(self):
+        self.daily_sales_volume = defaultdict(int)
+        self.daily_sales_value = defaultdict(float)
+        self.product_volumes = defaultdict(int)
+        self.monthly_staff_sales = defaultdict(lambda: defaultdict(int))
+        self.daily_avg_transactions = defaultdict(lambda: defaultdict(int))
+    
+    def parse_timestamp(self, timestamp):
+         # TODO: Implement timestamp parsing
+        return NotImplementedError("timestamp parsing not implemented yet")
+
+    
+    def parse_products(self, product_str):
+         # TODO: Implement product parsing
+        return NotImplementedError("product parsing not implemented yet")
+
+        
+    def process_line(self, line, filename):
+         # TODO: Implement process line
+        return NotImplementedError("line processing not implemented yet")
+
+    
+    def process_file(self, filepath):
+         # TODO: Implement process file
+        return NotImplementedError("File processing not implemented yet")
+
+    
+    def calculate_hourly_average(self, date_str):
+         # TODO: Implement hourly average
+        return NotImplementedError("Report generation not implemented yet")
+
+    def generate_report(self):
+        # TODO: Implement report generation
+        return NotImplementedError("hourly average not implemented yet")
+
+def analyze_transactions(base_path):
+     # TODO: Implement transaction analysis
+        return NotImplementedError("transaction analysis not implemented yet")
 
 if __name__ == "__main__":
-    main()
+    base_path = "./mp-hackathon-sample-data"
+    analyze_transactions(base_path)
